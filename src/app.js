976a3208c210e4b7617a146a01068dd0355570f4
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-12-09 22:02:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,21 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+         req.on('end', () => {
+
+        if (!obj || !obj.num1 || typeof obj.num1 !== 'number') {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid payload. Please provide a valid number in JSON format');
+
+        } else if (value % 2 == 0) {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`the number ${value} is even`);
+        } else {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end(`the number ${value} is odd`);
+        }
+
+      })
 
    });
   }
